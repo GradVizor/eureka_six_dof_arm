36d9b2beaa40ac9548ecd5ba586c4bce6c4c7ee4
Reishabh Rathore <129383936+GradVizor@users.noreply.github.com>
2024-10-20 16:46:56 +0530

    Update send_trajectory.launch.py

--- eureka/launch/send_trajectory.launch.py
+++ eureka/launch/send_trajectory.launch.py
@@ -2,23 +2,16 @@ from launch import LaunchDescription
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
+from ament_index_python.packages import get_package_share_directory
+import xacro
+import os
 
 def generate_launch_description():
     # Get URDF via xacro
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [
-                    FindPackageShare("eureka"),
-                    "urdf",
-                    "robot.xacro",
-                ]
-            ),
-        ]
-    )
+    share_dir = get_package_share_directory('eureka')
+    xacro_file = os.path.join(share_dir, 'urdf', 'robot.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_description_content = robot_description_config.toxml()
     robot_description = {"robot_description": robot_description_content}
 
     send_trajectory_node = Node(
